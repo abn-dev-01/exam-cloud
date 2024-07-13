c182182d30078dc6c3708dddaedbded0102b28bc
ANIKIN <abn.dev.01@gmail.com>
2024-07-13 21:13:16 +0300

    feat(discovery): integrate detekt for static code analysis
    - Add detekt plugin and configuration to the build.gradle.kts.
    - Include detekt static code analysis tool with specific rules and plugins.
    - Adjust Kotlin plugin versions and add new dependencies related to detekt.
    - Set up detekt in the detekt.yml configuration file for maintaining code quality.
    
    This integration aims to enhance code quality and maintain coding standards across the project.

--- discovery/build.gradle.kts
+++ discovery/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    id("org.springframework.boot") version "3.3.1"
-    id("io.spring.dependency-management") version "1.1.5"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    id("org.springframework.boot").version("3.3.1")
+    id("io.spring.dependency-management").version("1.1.5")
+    kotlin("jvm").version("1.9.23")
+    kotlin("plugin.spring").version("1.9.23")
+    id("io.gitlab.arturbosch.detekt").version("1.23.6")
 }
 
 group = "pro.abnjava.examcloud"
@@ -21,16 +22,31 @@ repositories {
 extra["springCloudVersion"] = "2023.0.2"
 
 dependencies {
+    implementation(kotlin("stdlib"))
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
     implementation("org.glassfish.jaxb:jaxb-runtime:4.0.5")
 
+    implementation("org.springframework.cloud:spring-cloud-starter")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     developmentOnly("org.springframework.boot:spring-boot-actuator-autoconfigure")
+//    implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+detekt {
+    toolVersion = "1.23.6"
+    config.setFrom(file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true // Adjust this based on your needs
+    allRules = false // Activate all available (even unstable) rules.
+    parallel = true // Uses current amount of cpu cores to speed up analysis.
+    baseline = file("config/detekt/baseline.xml") // Use if you have a baseline for ignored issues
 }
 
 dependencyManagement {
